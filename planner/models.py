b9d26e18356a7eb8654e7f1abc096ad2c9aa6d94
Chambo1804 <lin.180495@yandex.ru>
2022-04-12 17:51:15 +0300

    check

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class delo(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    time = models.DateTimeField(null=True)
+    importance = models.SmallIntegerField(null=True)
+
 # Create your models here.
--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 
+from .models import delo
+
+
 def index(request):
-    return HttpResponse("Cайт в разработке. Эля, жди!!!")
+    s = 'Дела на сегодня:\r\n'
+    for simpleDelo in delo.objects:
+        s += simpleDelo.title + "\r\n" + simpleDelo.content + '\r\n' + "Важность -" + simpleDelo.importance + "\r\n"
+    return HttpResponse(s, content_type='text/plain')
 
 # Create your views here.
