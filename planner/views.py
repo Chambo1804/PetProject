7ced9d2c93f2e3112cb4a534eb15aa070382df2b
Chambo1804 <lin.180495@yandex.ru>
2022-04-12 17:55:35 +0300

    check

--- planner/views.py
+++ planner/views.py
@@ -5,8 +5,8 @@ from .models import delo
 
 def index(request):
     s = 'Дела на сегодня:\r\n'
-    for simpleDelo in delo.objects:
-        s += simpleDelo.title + "\r\n" + simpleDelo.content + '\r\n' + "Важность -" + simpleDelo.importance + "\r\n"
-    return HttpResponse(s, content_type='text/plain')
+    for simpleDelo in delo.objects.all():
+        s += simpleDelo.title + "\r\n" + simpleDelo.content + '\r\n' + "Важность -" + str(simpleDelo.importance) + "\r\n"
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
 
 # Create your views here.
